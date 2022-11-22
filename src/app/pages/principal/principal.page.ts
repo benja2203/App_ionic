518acfa8953c12d7e04c889548ae0bfdae3c245b
benja2203 <barayafernandez@gmail.com>
2022-11-22 13:53:08 -0300

    First commit

--- src/app/pages/principal/principal.page.ts
+++ src/app/pages/principal/principal.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-principal',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalPage implements OnInit {
 
+  texto: string = '';
+
   constructor() { }
 
   ngOnInit() {
   }
+
+
+
+  async leerQR() {
+    document.querySelector('body').classList.add('scanner-active');
+
+    await BarcodeScanner.checkPermission({ force: true });
+
+    BarcodeScanner.hideBackground();
+
+    const result = await BarcodeScanner.startScan();
+
+    if (result.hasContent) {
+      this.texto = result.content;
+    }
+
+    document.querySelector('body').classList.remove('scanner-active');
+  };
 
 }
